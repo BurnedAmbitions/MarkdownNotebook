636fe19fc2715c1ad03a3538b412a3f016266952
ambitions <212634409+BurnedAmbitions@users.noreply.github.com>
2026-07-03 14:02:28 -0700

    - addition + organization of md files - changed default line settings

--- scripts/layout_color.py
+++ scripts/layout_color.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QWidget
 from PySide6.QtGui import QPalette, QColor
+from PySide6.QtWidgets import QWidget
+
 
 class Color(QWidget):
     def __init__(self, color):
--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import sys
+from PySide6.QtWidgets import QApplication
+from scripts.simpletexteditor import TextEditor
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#def main
+
+#directly from tutorial bc idk what this does
+def main():
+    app = QApplication(sys.argv)
+    window = TextEditor()
+    window.show()
+    app.exec()
+    pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    main()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
